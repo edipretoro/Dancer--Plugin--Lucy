c07b06a8b6dcb01c3ce5934a35e3d5d8302672f9
Emmanuel Di Pretoro <edipretoro@gmail.com>
2012-02-26 00:09:51 +0100

    * lib/Dancer/Plugin/Lucy.pm: first version of the indexer symbol

--- lib/Dancer/Plugin/Lucy.pm
+++ lib/Dancer/Plugin/Lucy.pm
@@ -9,7 +9,28 @@ use Lucy::Plan::FullTextType;
 use Lucy::Analysis::PolyAnalyzer;
 
 register indexer => sub {
+    my $conf = ${ plugin_setting() }{indexer};
+    my $schema = Lucy::Plan::Schema->new();
 
+    my $polyanalyser = Lucy::Analysis::PolyAnalyzer->new(
+        language => $conf->{polyanalyser}{language},
+    );
+
+    my $type = Lucy::Plan::FullTextType->new(
+        analyser => $polyanalyser,
+    );
+
+    foreach my $field ( @{ $conf->{schema}{fields}} ) {
+        $schema->spec_field( name => $field, type => $type );
+    }
+
+    my $indexer = Lucy::Index::Indexer->new(
+        schema => $schema,
+        index => ${ plugin_setting() }{index},
+        create => $conf->{create},
+    );
+
+    return $indexer;
 };
 
 register searcher => sub {
