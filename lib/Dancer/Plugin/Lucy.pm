635ec5d28146d73e2ef958ffa4e3f90746c15c8f
Emmanuel Di Pretoro <edipretoro@gmail.com>
2012-02-25 23:25:44 +0100

    * lib/Dancer/Plugin/Lucy.pm: add the requirements for the indexer

--- lib/Dancer/Plugin/Lucy.pm
+++ lib/Dancer/Plugin/Lucy.pm
@@ -3,6 +3,10 @@ use warnings;
 package Dancer::Plugin::Lucy;
 use Dancer ':syntax';
 use Dancer::Plugin;
+use Lucy::Index::Indexer;
+use Lucy::Plan::Schema;
+use Lucy::Plan::FullTextType;
+use Lucy::Analysis::PolyAnalyzer;
 
 register indexer => sub {
 
