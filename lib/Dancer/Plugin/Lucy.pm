7f23be10053c1602cdf268abc8747ec52d18b025
Emmanuel Di Pretoro <edipretoro@gmail.com>
2012-09-29 11:32:44 +0200

    * lib/Dancer/Plugin/Lucy.pm: using the full Lucy ecosystem, again...

--- lib/Dancer/Plugin/Lucy.pm
+++ lib/Dancer/Plugin/Lucy.pm
@@ -6,14 +6,34 @@ package Dancer::Plugin::Lucy;
 use Dancer ':syntax';
 use Dancer::Plugin;
 
-use Lucy::Simple;
+use Lucy::Index::Indexer;
+use Lucy::Plan::Schema;
+use Lucy::Plan::FullTextType;
+use Lucy::Analysis::PolyAnalyzer;
+
+use Lucy::Search::IndexSearcher;
 
 register indexer => sub {
     my $conf = plugin_setting();
+    my $schema = Lucy::Plan::Schema->new();
 
-    my $indexer = Lucy::Simple->new(
-        path => $conf->{index},
+    my $polyanalyser = Lucy::Analysis::PolyAnalyzer->new(
         language => $conf->{polyanalyser}{language},
+    );
+
+    my $type = Lucy::Plan::FullTextType->new(
+        analyzer => $polyanalyser,
+    );
+
+    foreach my $field ( @{ $conf->{schema}{fields}} ) {
+        $schema->spec_field( name => $field, type => $type );
+    }
+
+    my $indexer = Lucy::Index::Indexer->new(
+        schema => $schema,
+        index => $conf->{index},
+        create => $conf->{create} || 0,
+        truncate => $conf->{truncate} || 0,
     );
 
     return $indexer;
@@ -21,9 +41,8 @@ register indexer => sub {
 
 register searcher => sub {
     my $conf = plugin_setting();
-    my $searcher = Lucy::Simple->new(
-        path => $conf->{index},
-        language => $conf->{polyanalyser}{language},
+    my $searcher = Lucy::Search::IndexSearcher->new(
+        index => $conf->{index},
     );
 
     return $searcher;
